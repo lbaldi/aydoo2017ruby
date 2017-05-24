43798262ca4cc9e2260d2082944e13ed9537e194
Leandro Ezequiel Baldi <lbaldi@openpyme.com.ar>
2017-05-23 21:48:38 -0300

    Factorize method and test

--- factores-primos-4/model/prime_factors_program.rb
+++ factores-primos-4/model/prime_factors_program.rb
@@ -1,6 +1,25 @@
 # Permite obtener los factores primos de un numero
 class PrimeFactorsProgram
+  BASE_PRIME_FACTOR = 2
+
+  def self.validate_number(number)
+    message_error = 'Valor ingresado incorrecto. Debe ingresar un numero natural mayor igual a dos.'
+    raise ArgumentError, message_error if number < BASE_PRIME_FACTOR
+  end
+
+  # Dado un numero calcula los factores y los retorna en una lista
+  # Parametros:
+  # +number+:: numero sobre el cual se obtendran los factores
   def self.factorize(number)
-    []
+    validate_number(number)
+    factor = BASE_PRIME_FACTOR
+    factors = []
+    while number % factor != 0 do
+      factor += 1
+    end
+    factors.push(factor)
+    remaining = number / factor
+    factors += factorize(remaining) if remaining >= BASE_PRIME_FACTOR
+    factors
   end
 end
--- factores-primos-4/spec/prime_factors_program_spec.rb
+++ factores-primos-4/spec/prime_factors_program_spec.rb
@@ -2,10 +2,21 @@ require 'rspec'
 require_relative '../model/prime_factors_program'
 
 describe 'Prime Factors Program' do
-
   let(:primeFactorProgram) { PrimeFactorsProgram }
-  it 'Factorize 0 should return an empty list' do
-    expect(primeFactorProgram.factorize(0)).to eq []
+
+  it 'Factorize 2 should return [2]' do
+    expect(primeFactorProgram.factorize(2)).to eq [2]
   end
 
+  it 'Factorize 4 should return [2, 2]' do
+    expect(primeFactorProgram.factorize(4)).to eq [2, 2]
+  end
+
+  it 'Factorize 90 should return [2, 3, 3, 5]' do
+    expect(primeFactorProgram.factorize(90)).to eq [2, 3, 3, 5]
+  end
+
+  it 'Factorize 360 should return [2, 2, 2, 3, 3, 5]' do
+    expect(primeFactorProgram.factorize(360)).to eq [2, 2, 2, 3, 3, 5]
+  end
 end
